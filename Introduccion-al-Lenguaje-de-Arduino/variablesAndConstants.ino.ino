e0597d68fd2ca4cecd3cc49a7717191624768acb
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2021-05-08 09:04:41 -0600

    [Add] - Variables & constants

---
+++ Introduccion-al-Lenguaje-de-Arduino/variablesAndConstants.ino.ino
@@ -0,0 +1,16 @@
+bool flag = false;
+byte data = 255;
+short short_number = -32768;
+word word_number = 65535;
+int number = -32768;
+unsigned int unumber = 65535;
+long int long_number = -2147483648;
+unsigned long int ulong_number = 4294967295;
+float float_number = -3.4028235e20;
+double double_number = -3.4028335e20;
+
+void setup() {
+}
+
+void loop() {
+}
